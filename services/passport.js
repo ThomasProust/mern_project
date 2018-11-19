b30f7bca68d84009b6d3d05e7b9bf026b81b36d9
Thomas Proust <thomas@zla.io>
2018-11-19 13:40:51 +0900

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true //trudt heroku proxy
 		}, 
 		(accessToken, refreshToken, profile, done) => {
 
